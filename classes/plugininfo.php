40f9650595256e465b511003f178236942a7cc7c
GGeorggg <georg-github.com@glas.eu.org>
2026-08-26 16:49:34 +0200

    Fix: Avoid unused parameters / plugininfo::get_plugin_configuration_for_context has incomplete parameters list (error)
    https://github.com/center-for-learning-management/moodle-tiny_fontfamily/issues/12

--- classes/plugininfo.php
+++ classes/plugininfo.php
@@ -60,13 +60,17 @@ class plugininfo extends plugin implements plugin_with_buttons, plugin_with_conf
      * Get plugin configuration.
      * Currently not in use.
      *
+     * @param context $context The context
+     * @param array $options The options
+     * @param array $fpoptions The filepicker options
+     * @param \editor_tiny\editor|null $editor The editor instance
      * @return array
      */
     public static function get_plugin_configuration_for_context(
-        context $context,
-        array $options,
-        array $fpoptions,
-        ?\editor_tiny\editor $editor = null
+        $_context: context,
+        array $_options,
+        array $_fpoptions,
+        ?\editor_tiny\editor $_editor = null
     ): array {
         $config = [];
         $config['fonts'] = explode("\r\n", get_config('tiny_fontfamily', 'fonts'));
